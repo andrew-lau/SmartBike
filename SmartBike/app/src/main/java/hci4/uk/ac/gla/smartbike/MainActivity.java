a943f064a737dd258ce2fcef970ec893ee241983
andrew-lau <andrewlau@salisbury.dcs.gla.ac.uk>
2014-11-20 19:19:30 +0000

    Add project

--- SmartBike/app/src/main/java/hci4/uk/ac/gla/smartbike/MainActivity.java
+++ SmartBike/app/src/main/java/hci4/uk/ac/gla/smartbike/MainActivity.java
@@ -1,28 +1,98 @@
 package hci4.uk.ac.gla.smartbike;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.pm.ActivityInfo;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.apache.commons.logging.Log;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private LocationClient locationClient;
+    private LocationRequest locationRequest;
+    private GoogleMap googleMap;
+    private Marker marker;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+
+        // configure location
+        locationClient = new LocationClient(this, this, this);
+        locationRequest = LocationRequest.create();
+
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(100);
+        locationRequest.setFastestInterval(100);
+
+        locationClient.connect();
+
+
+        // configure map
+        FragmentManager parentFragmentManager = getFragmentManager();
+        Fragment wrapper = parentFragmentManager.findFragmentById(R.id.mapwrapper);
+        FragmentManager childFragmentManager = wrapper.getChildFragmentManager();
+
+        MapFragment mapFragment = (MapFragment) childFragmentManager.findFragmentById(R.id.map22);
+
+        MapsInitializer.initialize(this);
+
+        googleMap = mapFragment.getMap();
+        if (googleMap == null){
+            System.out.println("yo");
         }
+
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(55.8554602,-4.2324586))
+                .zoom(19)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(70)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        googleMap.setBuildingsEnabled(true);
+
+        marker = googleMap.addMarker(new MarkerOptions().position(new LatLng(55.8554602,-4.2324586)));
+
+
+    }
+
+    @Override
+    protected void onDestroy(){
+       locationClient.disconnect();
+        super.onDestroy();
     }
 
 
@@ -48,19 +118,25 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    @Override
+    public void onConnected(Bundle bundle) {
+        locationClient.requestLocationUpdates(locationRequest, this);
+    }
 
-        public PlaceholderFragment() {
-        }
+    @Override
+    public void onDisconnected() {
+    }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null){
+            googleMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+            marker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
         }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
---
+++ SmartBike/app/src/main/java/hci4/uk/ac/gla/smartbike/MapWrapperFragment.java
@@ -0,0 +1,21 @@
+package hci4.uk.ac.gla.smartbike;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by andrewlau on 20/11/2014.
+ */
+public class MapWrapperFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.map_fragment, container, false);
+    }
+
+}
